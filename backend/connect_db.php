5e80e5f8b4aefd662f224a6a4f9c84e73e3007a2
vhika31 <115438373+vhika31@users.noreply.github.com>
2022-11-21 22:56:40 +0700

    mengkoneksikan ke database uas_presensi_vhikawanasa

--- backend/connect_db.php
+++ backend/connect_db.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "backend_webpro";
+$dbname = "uas_presensi_vhikawanasa";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
